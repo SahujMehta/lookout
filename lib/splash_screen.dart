fb432caaca8d96d8fee47372447059cbc090c3fd
ssmehta <ssmehta@crimson.ua.edu>
2023-03-01 20:48:07 -0600

    Splash Screen settings

---
+++ lib/splash_screen.dart
@@ -0,0 +1,43 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, '/home');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xff241181),
+      body: Center(
+        child: SizedBox(
+          width: 448.0,
+          height: 125.0,
+          child: Text(
+            'LookOut',
+            style: TextStyle(
+              fontFamily: 'Arial Rounded MT',
+              fontSize: 60,
+              color: const Color(0xffffffff),
+              fontWeight: FontWeight.w700,
+            ),
+            textAlign: TextAlign.center,
+            softWrap: false,
+          ),
+        ),
+      ),
+    );
+  }
+}
