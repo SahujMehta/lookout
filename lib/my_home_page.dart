42b22d11dd86e24460a5902753c87dc7323cae7e
sascott7 <sascott7@crimson.ua.edu>
2023-03-03 17:39:25 -0600

    amplify integration

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -2,6 +2,13 @@ import 'package:flutter/material.dart';
 import 'contacts.dart';
 import 'help.dart';
 import 'settings.dart';
+import 'dart:async';
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:amplify_datastore/amplify_datastore.dart';
+import 'amplifyconfiguration.dart';
+import 'models/ModelProvider.dart';
+import 'package:intl/intl.dart';
+import 'package:date_format/date_format.dart';
 
 class MyHomePage extends StatefulWidget {
   @override
@@ -65,6 +72,44 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  late StreamSubscription<QuerySnapshot<TimedLookout>> _subscription;
+  List<TimedLookout> _timedLookouts = [];
+
+  @override
+  void initState() {
+    // kick off app initialization
+    _initializeApp();
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _subscription.cancel();
+  }
+
+  Future<void> _initializeApp() async {
+    await _configureAmplify();
+    _subscription = Amplify.DataStore.observeQuery(TimedLookout.classType)
+        .listen((QuerySnapshot<TimedLookout> snapshot) {
+      setState(() {
+        _timedLookouts = snapshot.items;
+      });
+    });
+  }
+
+  Future<void> _configureAmplify() async {
+    try {
+      final _dataStorePlugin =
+          AmplifyDataStore(modelProvider: ModelProvider.instance);
+      await Amplify.addPlugins([_dataStorePlugin]);
+      await Amplify.configure(amplifyconfig);
+    } catch (e) {
+      safePrint('An error occurred while configuring Amplify: $e');
+    }
+  }
+
   final List<String> items = [];
 
   void addItem() {
@@ -84,7 +129,13 @@ class _HomeState extends State<Home> {
           width: double.infinity,
           height: 80,
           child: ElevatedButton(
-            onPressed: addItem,
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const AddTimedLookout()),
+              );
+            },
             child: Text(
               'Start Timed LookOut',
               style: TextStyle(
@@ -112,22 +163,288 @@ class _HomeState extends State<Home> {
         SizedBox(height: 16), // add some spacing between buttons
         Expanded(
           child: Container(
-            padding: const EdgeInsets.all(8.0),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: Colors.grey[200],
-            ),
-            child: ListView.builder(
-              itemCount: items.length,
-              itemBuilder: (BuildContext context, int index) {
-                return ListTile(
-                  title: Text(items[index]),
-                );
-              },
-            ),
-          ),
+              padding: const EdgeInsets.all(8.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.grey[200],
+              ),
+              child: ListView(
+                  children: _timedLookouts
+                      .map((timedLookout) =>
+                          TimedLookoutTimer(timedLookout: timedLookout))
+                      .toList())),
         ),
       ],
+    );
+  }
+}
+
+class TimedLookoutTimer extends StatelessWidget {
+  const TimedLookoutTimer({
+    required this.timedLookout,
+    Key? key,
+  }) : super(key: key);
+
+  final double iconSize = 24.0;
+  final TimedLookout timedLookout;
+
+  void _deleteTimedLookout(BuildContext context) async {
+    try {
+      await Amplify.DataStore.delete(timedLookout);
+    } catch (e) {
+      safePrint('An error occurred while deleting Timed Lookout: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: InkWell(
+        onTap: () {
+          _deleteTimedLookout(context);
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    timedLookout.title,
+                    style: const TextStyle(
+                        fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    DateTime.parse(timedLookout.expire_time.toString())
+                        .toLocal()
+                        .toString(),
+                  ),
+                  Text(timedLookout.description ?? ''),
+                ],
+              ),
+            ),
+          ]),
+        ),
+      ),
+    );
+  }
+}
+
+class AddTimedLookout extends StatefulWidget {
+  const AddTimedLookout({Key? key}) : super(key: key);
+
+  @override
+  State<AddTimedLookout> createState() => _AddTimedLookoutState();
+}
+
+class _AddTimedLookoutState extends State<AddTimedLookout> {
+  late final TextEditingController _titleController;
+  late final TextEditingController _descriptionController;
+  late final TextEditingController _dateController;
+  late final TextEditingController _timeController;
+
+  late double _height;
+  late double _width;
+
+  late String _setTime, _setDate;
+
+  late String _hour, _minute, _time;
+
+  late String dateTime;
+
+  DateTime selectedDate = DateTime.now();
+
+  TimeOfDay selectedTime = TimeOfDay(hour: 00, minute: 00);
+
+  @override
+  void initState() {
+    _titleController = TextEditingController();
+    _descriptionController = TextEditingController();
+    _dateController = TextEditingController();
+    _timeController = TextEditingController();
+    _dateController.text = DateFormat.yMd().format(DateTime.now());
+    _timeController.text = formatDate(
+        DateTime(DateTime.now().hour, DateTime.now().minute),
+        [hh, ':', nn, " ", am]).toString();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _titleController.dispose();
+    _descriptionController.dispose();
+    _dateController.dispose();
+  }
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+        context: context,
+        initialDate: selectedDate,
+        initialDatePickerMode: DatePickerMode.day,
+        firstDate: DateTime(2015),
+        lastDate: DateTime(2101));
+    if (picked != null) {
+      setState(() {
+        selectedDate = picked;
+        _dateController.text = DateFormat.yMd().format(selectedDate);
+      });
+    }
+  }
+
+  Future<void> _selectTime(BuildContext context) async {
+    final TimeOfDay? picked = await showTimePicker(
+      context: context,
+      initialTime: selectedTime,
+    );
+    if (picked != null) {
+      setState(() {
+        selectedTime = picked;
+        _hour = selectedTime.hour.toString();
+        _minute = selectedTime.minute.toString();
+        _time = _hour + ' : ' + _minute;
+        _timeController.text = _time;
+        _timeController.text = formatDate(
+            DateTime(2019, 08, 1, selectedTime.hour, selectedTime.minute),
+            [hh, ':', nn, " ", am]).toString();
+      });
+    }
+  }
+
+  Future<void> _saveTimedLookout() async {
+    // get the current text field contents
+    final title = _titleController.text;
+    final description = _descriptionController.text;
+    final date = _dateController.text;
+    final time = _timeController.text;
+    final formatedDate = DateFormat('MM/dd/yyyy hh:mm a').parse('$date $time');
+    // create a new Todo from the form values
+    // `isComplete` is also required, but should start false in a new Todo
+    final newTimer = TimedLookout(
+      title: title,
+      expire_time: TemporalDateTime(formatedDate),
+      description: description.isNotEmpty ? description : null,
+    );
+    try {
+      // to write data to DataStore, you simply pass an instance of a model to
+      // Amplify.DataStore.save()
+      await Amplify.DataStore.save(newTimer);
+      // after creating a new Todo, close the form
+      // Be sure the context at that moment is still valid and mounted
+      if (mounted) {
+        Navigator.of(context).pop();
+      }
+    } catch (e) {
+      safePrint('An error occurred while saving Timed Lookout: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _height = MediaQuery.of(context).size.height;
+    _width = MediaQuery.of(context).size.width;
+    dateTime = DateFormat.yMd().format(DateTime.now());
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Add Timed Lookout',
+          style: TextStyle(
+              fontSize: 20, fontWeight: FontWeight.bold, fontFamily: 'Roboto'),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextFormField(
+                controller: _titleController,
+                decoration:
+                    const InputDecoration(filled: true, labelText: 'Name'),
+              ),
+              TextFormField(
+                controller: _descriptionController,
+                decoration: const InputDecoration(
+                    filled: true, labelText: 'Description'),
+              ),
+              const Text(
+                'Choose Date',
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Roboto'),
+              ),
+              InkWell(
+                onTap: () {
+                  _selectDate(context);
+                },
+                child: Container(
+                  width: _width / 1.7,
+                  height: _height / 9,
+                  margin: EdgeInsets.only(top: 15),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(color: Colors.grey[200]),
+                  child: TextFormField(
+                    style: const TextStyle(fontSize: 40),
+                    textAlign: TextAlign.center,
+                    enabled: false,
+                    keyboardType: TextInputType.text,
+                    controller: _dateController,
+                    onSaved: (String? val) {
+                      _setDate = val!;
+                    },
+                    decoration: const InputDecoration(
+                        disabledBorder:
+                            UnderlineInputBorder(borderSide: BorderSide.none),
+                        // labelText: 'Time',
+                        contentPadding: EdgeInsets.only(top: 0.0)),
+                  ),
+                ),
+              ),
+              const Text(
+                'Choose Time',
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Roboto'),
+              ),
+              InkWell(
+                onTap: () {
+                  _selectTime(context);
+                },
+                child: Container(
+                  margin: EdgeInsets.only(top: 15),
+                  width: _width / 1.7,
+                  height: _height / 9,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(color: Colors.grey[200]),
+                  child: TextFormField(
+                    style: TextStyle(fontSize: 40),
+                    textAlign: TextAlign.center,
+                    onSaved: (String? val) {
+                      _setTime = val!;
+                    },
+                    enabled: false,
+                    keyboardType: TextInputType.text,
+                    controller: _timeController,
+                    decoration: const InputDecoration(
+                        disabledBorder:
+                            UnderlineInputBorder(borderSide: BorderSide.none),
+                        // labelText: 'Time',
+                        contentPadding: EdgeInsets.all(5)),
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: _saveTimedLookout,
+                child: const Text('Start'),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
