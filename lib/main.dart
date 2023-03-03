bdc866a4328ada07249295e6b6bc8c5fb9a578d6
Anderson <abliddle@crimson.ua.edu>
2023-03-03 16:47:32 -0600

    Updated minSdkVersion to 21 and added example AWS Amplify code. Will delete later

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,23 @@ import 'help.dart';
 import 'settings.dart';
 import 'package:permission_handler/permission_handler.dart';
 
+//**** AWS Amplify ****
+// dart async library you will refer to when setting up real time updates
+import 'dart:async';
+
+// flutter and ui libraries
+import 'package:flutter/material.dart';
+
+// amplify packages you will need to use
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:amplify_datastore/amplify_datastore.dart';
+
+// amplify configuration and models that should have been generated for you
+import 'amplifyconfiguration.dart';
+import 'models/ModelProvider.dart';
+import 'models/Todo.dart';
+//****    ****
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Permission.contacts.request();
@@ -27,6 +44,200 @@ class MyApp extends StatelessWidget {
         '/help': (context) => Help(),
         '/settings': (context) => Settings(),
       },
+    );
+  }
+}
+
+//**** AWS Amplify ****
+class TodosPage extends StatefulWidget {
+  const TodosPage({Key? key}) : super(key: key);
+
+  @override
+  State<TodosPage> createState() => _TodosPageState();
+}
+
+class _TodosPageState extends State<TodosPage> {
+  @override
+  void initState() {
+    // to be filled in a later step
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    // to be filled in a later step
+  }
+
+  Future<void> _initializeApp() async {
+    // to be filled in a later step
+  }
+
+  Future<void> _configureAmplify() async {
+    // to be filled in a later step
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Todo List'),
+      ),
+      body: const Center(child: CircularProgressIndicator()),
+      // body: _isLoading
+      //     ? Center(child: CircularProgressIndicator())
+      //     : TodosList(todos: _todos),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const AddTodoForm()),
+          );
+        },
+        tooltip: 'Add Todo',
+        label: Row(
+          children: const [Icon(Icons.add), Text('Add todo')],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
+
+class TodosList extends StatelessWidget {
+  const TodosList({
+    required this.todos,
+    Key? key,
+  }) : super(key: key);
+
+  final List<Todo> todos;
+
+  @override
+  Widget build(BuildContext context) {
+    return todos.isNotEmpty
+        ? ListView(
+            padding: const EdgeInsets.all(8),
+            children: todos.map((todo) => TodoItem(todo: todo)).toList())
+        : const Center(
+            child: Text('Tap button below to add a todo!'),
+          );
+  }
+}
+
+class TodoItem extends StatelessWidget {
+  const TodoItem({
+    required this.todo,
+    Key? key,
+  }) : super(key: key);
+
+  final double iconSize = 24.0;
+  final Todo todo;
+
+  void _deleteTodo(BuildContext context) async {
+    // to be filled in a later step
+  }
+
+  Future<void> _toggleIsComplete() async {
+    // to be filled in a later step
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: InkWell(
+        onTap: () {
+          _toggleIsComplete();
+        },
+        onLongPress: () {
+          _deleteTodo(context);
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    todo.name,
+                    style: const TextStyle(
+                        fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  Text(todo.description ?? 'No description'),
+                ],
+              ),
+            ),
+            Icon(
+                todo.isComplete ?? false
+                    ? Icons.check_box
+                    : Icons.check_box_outline_blank,
+                size: iconSize),
+          ]),
+        ),
+      ),
+    );
+  }
+}
+
+class AddTodoForm extends StatefulWidget {
+  const AddTodoForm({Key? key}) : super(key: key);
+
+  @override
+  State<AddTodoForm> createState() => _AddTodoFormState();
+}
+
+class _AddTodoFormState extends State<AddTodoForm> {
+  late final TextEditingController _nameController;
+  late final TextEditingController _descriptionController;
+
+  @override
+  void initState() {
+    _nameController = TextEditingController();
+    _descriptionController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _nameController.dispose();
+    _descriptionController.dispose();
+  }
+
+  Future<void> _saveTodo() async {
+    // to be filled in a later step
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add Todo'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextFormField(
+                controller: _nameController,
+                decoration:
+                    const InputDecoration(filled: true, labelText: 'Name'),
+              ),
+              TextFormField(
+                controller: _descriptionController,
+                decoration: const InputDecoration(
+                    filled: true, labelText: 'Description'),
+              ),
+              ElevatedButton(
+                onPressed: _saveTodo,
+                child: const Text('Save'),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
